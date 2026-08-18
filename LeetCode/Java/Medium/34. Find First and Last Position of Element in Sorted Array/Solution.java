b53ae9aaa6c17e007dbc780073a27d7d4a1f2fdd
Rajavardhan <rajavardhankuraku@gmail.com>
2026-08-18 17:28:47 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/34. Find First and Last Position of Element in Sorted Array/Solution.java
+++ LeetCode/Java/Medium/34. Find First and Last Position of Element in Sorted Array/Solution.java
@@ -21,7 +21,7 @@ class Solution {
                         return a;
                     }
                     else{
-                        low;
+                        low=low+1;
                     }
                 }
             }
