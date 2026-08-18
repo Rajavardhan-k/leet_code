174a03dd5e14877ec4062fb2c334670d004f8afe
Rajavardhan <rajavardhankuraku@gmail.com>
2026-08-18 16:16:19 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/374. Guess Number Higher or Lower/Solution.java
+++ LeetCode/Java/Easy/374. Guess Number Higher or Lower/Solution.java
@@ -13,14 +13,13 @@ public class Solution extends GuessGame {
         int high=n;
         while(low<=high){
             int mid=low+((high-low)/2);
-            int check=guess(mid);
-            if(check==-1){
+            if(guess(mid)==-1){
                 high=mid-1;
             }
-            else if(check==1){
+            else if(guess(mid)==1){
                 low=mid+1;
             }
-            else if(check==0){
+            else if(guess(mid)==0){
                 return mid;
             }
         }
