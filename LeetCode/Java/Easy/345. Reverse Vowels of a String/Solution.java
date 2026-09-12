fbe784803c551c1980bbe49035100f18e964816f
Rajavardhan <rajavardhankuraku@gmail.com>
2026-09-12 09:56:08 +0530

    Add Easy Java Solution for 345. Reverse Vowels of a String from LeetCode

---
+++ LeetCode/Java/Easy/345. Reverse Vowels of a String/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String reverseVowels(String s) {
+        int i=0;
+        int j=s.length()-1;
+        String vowels = "aeiouAEIOU";
+        char[] arr = s.toCharArray();
+        while(i<=j){
+            if (vowels.indexOf(arr[i]) == -1) {
+                i++;
+                continue;
+            } 
+            else if (vowels.indexOf(arr[j]) == -1) {
+                j--;
+                continue;
+            }
+            else{
+                char temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j--;
+            }
+        }
+        return new String(arr);
+    }
+}
