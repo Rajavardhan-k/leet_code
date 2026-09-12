568e52fc4da98f5c5fb052fbd8dba67ee4e44723
Rajavardhan <rajavardhankuraku@gmail.com>
2026-09-12 10:52:07 +0530

    Add Easy Java Solution for 925. Long Pressed Name from LeetCode

--- LeetCode/Patterns/Two Pointers/925. Long Pressed Name/Solution.java
+++ LeetCode/Patterns/Two Pointers/925. Long Pressed Name/Solution.java
@@ -16,8 +16,9 @@ class Solution {
             else return false;
         }
         while(j<arr2.length){
-            if()
-            return false;
+            if(arr2[j]!=arr2[j-1]){
+                return false;
+            }
         }
         return true;
     }
