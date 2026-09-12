c98235ea8e942451e596c3190883a85932c22242
Rajavardhan <rajavardhankuraku@gmail.com>
2026-09-12 11:26:12 +0530

    Add Easy Java Solution for 961. Long Pressed Name from LeetCode

--- LeetCode/Patterns/Two Pointers/961. Long Pressed Name/Solution.java
+++ LeetCode/Patterns/Two Pointers/961. Long Pressed Name/Solution.java
@@ -2,15 +2,17 @@ class Solution {
     public boolean isLongPressedName(String name, String typed) {
         int i=0;
         int j=0;
+        int temp1=0;
         char[] arr1 = name.toCharArray();
         char[] arr2 = typed.toCharArray();
         if(arr1.length>arr2.length) return false;
         while(i<arr1.length&&j<arr2.length){
+            temp1=j;
             if(arr1[i]==arr2[j]){
                 i++;
                 j++;
             }
-            else if(j>0 &&arr2[j]==arr2[j-1]){
+            else if(j>0 && arr2[temp1]==arr2[j]){
                 j++;
             }
             else return false;
