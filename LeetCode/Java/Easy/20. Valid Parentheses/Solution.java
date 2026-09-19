9d9c74c60ae084db3f20c644a321cf3df9a3423d
Rajavardhan <rajavardhankuraku@gmail.com>
2026-09-19 10:45:14 +0530

    Add Easy Java Solution for 20. Valid Parentheses from LeetCode

--- LeetCode/Java/Easy/20. Valid Parentheses/Solution.java
+++ LeetCode/Java/Easy/20. Valid Parentheses/Solution.java
@@ -1,22 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
-                st.push(s.charAt(i));
-            }
-            else{
-                if(!st.isEmpty()&&s.charAt(i)==')'&&st.peek()=='('){
-                    st.pop();
-                }
-                else if(!st.isEmpty()&&s.charAt(i)=='}'&&st.peek()=='{'){
-                    st.pop();
-                }
-                else if(!st.isEmpty()&&s.charAt(i)==']'&&st.peek()=='['){
-                    st.pop();
-                }
-            }
+        while (s.contains("()")||s.contains("{}")||s.contains("[]")) {
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
         }
-        return st.isEmpty();
+        return s.isEmpty();
     }
 }
