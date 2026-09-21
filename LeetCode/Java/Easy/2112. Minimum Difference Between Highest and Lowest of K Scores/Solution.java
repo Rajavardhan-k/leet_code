f69b9e09caae895cf4672551dd6e4c9b20218e7b
Rajavardhan <rajavardhankuraku@gmail.com>
2026-09-21 12:02:09 +0530

    Add Easy Java Solution for 2112. Minimum Difference Between Highest and Lowest of K Scores from LeetCode

---
+++ LeetCode/Java/Easy/2112. Minimum Difference Between Highest and Lowest of K Scores/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int diff;
+        if(k==1){
+            diff=0;
+        }
+        else {
+            diff=nums[nums.length-1]-nums[nums.length-2];
+        }
+        return diff;
+    }
+}
