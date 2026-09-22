7e99ba16fd467fce12c650e084bb0481fcefcfc2
Rajavardhan <rajavardhankuraku@gmail.com>
2026-09-22 16:36:06 +0530

    Add Easy Java Solution for 27. Remove Element from LeetCode

--- LeetCode/Patterns/Two Pointers/27. Remove Element/Solution.java
+++ LeetCode/Patterns/Two Pointers/27. Remove Element/Solution.java
@@ -1,9 +1,23 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        int i=0;
+        int j=0;
         int k=0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
+        int temp=0;
+        while(j<nums.length){
+            if(nums[j]==val){
+                j++;
+            }
+            else{
+                temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
+                j++;
+            }
+        }
+        for(i=0;i<nums.length;i++){
+            if(nums[i]!=val){
                 k++;
             }
         }
